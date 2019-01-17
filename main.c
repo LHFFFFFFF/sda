b9e261ac01e207051bee1daf98ddb2153c8332dc
Your Name <you@example.com>
2019-01-17 10:49:39 +0800

    应用框架

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 
-int main(){
 
-    printf("hello world!");
+int main(){
+{
+    printf("huanyingjinruzhuanchangweizhiguanlixitong!\n");
+
+    while(1)
+    {
+        printf("1---tianjiacheliangweizhi\n");
+        printf("2---shanchucheliangweizhi\n");
+        printf("3---xianshiquanbucheliangweizhi\n");
+        printf("4---fanhuiliwodeweizhizuijindecheliang\n");
+        printf("5---tuichu\n");
+
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            printf("yingyongyituichu\n");
+            break;
+        }
+    }
+}
 
     return 0;
 
